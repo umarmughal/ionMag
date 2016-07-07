810397415bba1ed1f2b38ad41c020bf8ac497e23
cristeaioan <c.ioan12@yahoo.com>
2016-07-07 11:27:18 +0300

    Submenu fixes & footer progress

--- parts/footer/td_footer_template_1.php
+++ parts/footer/td_footer_template_1.php
@@ -1,23 +1,23 @@
 <div class="td-footer-wrapper">
-    <div class="td-container">
-        <div class="td-pb-row">
-            <div class="td-pb-span4">
-                <?php dynamic_sidebar('Footer 1'); ?>
-            </div>
-
-            <div class="td-pb-span4">
-                <?php dynamic_sidebar('Footer 2'); ?>
-            </div>
-
-            <div class="td-pb-span4">
-                <?php dynamic_sidebar('Footer 3'); ?>
-            </div>
-        </div>
-    </div>
+<!--    <div class="td-container">-->
+<!--        <div class="td-pb-row">-->
+<!--            <div class="td-pb-span4">-->
+<!--                --><?php //dynamic_sidebar('Footer 1'); ?>
+<!--            </div>-->
+<!---->
+<!--            <div class="td-pb-span4">-->
+<!--                --><?php //dynamic_sidebar('Footer 2'); ?>
+<!--            </div>-->
+<!---->
+<!--            <div class="td-pb-span4">-->
+<!--                --><?php //dynamic_sidebar('Footer 3'); ?>
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
     <div class="td-footer-bottom-full">
         <div class="td-container">
             <div class="td-pb-row">
-                <?php //locate_template('parts/footer/td_footer_extra_bottom.php', true); ?>
+                <?php locate_template('parts/footer/td_footer_extra_bottom.php', true); ?>
             </div>
         </div>
     </div>
