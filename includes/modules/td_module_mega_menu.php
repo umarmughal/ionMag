c2ad3036ad9c4d79c5a5cfe72ec5b499eb74a639
cristeaioan <c.ioan12@yahoo.com>
2016-07-11 09:23:27 +0300

    Various fixes

--- includes/modules/td_module_mega_menu.php
+++ includes/modules/td_module_mega_menu.php
@@ -14,7 +14,7 @@ class td_module_mega_menu extends td_module {
         <div class="<?php echo $this->get_module_classes(array("td_mod_mega_menu")); ?>">
             <div class="td-module-image">
                 <?php echo $this->get_image('td_260x195');?>
-                <?php if (td_util::get_option('tds_category_module_mega_menu') == 'yes') { echo $this->get_category(); }?>
+                <div class="td-post-category-wrap"><?php if (td_util::get_option('tds_category_module_mega_menu') == 'yes') { echo $this->get_category(); }?></div>
             </div>
 
             <div class="item-details">
