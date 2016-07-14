66e3d40368cda7cf59529f00cbbcb1ac6390d598
marius <pmarius05@yahoo.com>
2016-07-14 10:06:23 +0300

    login modal

--- includes/td_css_generator.php
+++ includes/td_css_generator.php
@@ -40,6 +40,31 @@ function td_css_generator() {
         opacity: @footer_background_opacity;
     }
 
+    /* @login_background_image */
+    .white-popup-block:before {
+        background-image: url('@login_background_image');
+    }
+
+    /* @login_background_repeat */
+    .white-popup-block:before {
+        background-repeat: @login_background_repeat;
+    }
+
+    /* @login_background_size */
+    .white-popup-block:before {
+        background-size: @login_background_size;
+    }
+
+    /* @login_background_position */
+    .white-popup-block:before {
+        background-position: @login_background_position;
+    }
+
+    /* @login_background_opacity */
+    .white-popup-block:before {
+        opacity: @login_background_opacity;
+    }
+
     </style>
     ";
 
@@ -71,6 +96,13 @@ function td_css_generator() {
     $td_css_compiler->load_setting('footer_background_size');
     $td_css_compiler->load_setting('footer_background_position');
     $td_css_compiler->load_setting('footer_background_opacity');
+
+    // sign in/join background
+    $td_css_compiler->load_setting('login_background_image');
+    $td_css_compiler->load_setting('login_background_repeat');
+    $td_css_compiler->load_setting('login_background_size');
+    $td_css_compiler->load_setting('login_background_position');
+    $td_css_compiler->load_setting('login_background_opacity');
 
     //output the style
     return $td_css_compiler->compile_css();
