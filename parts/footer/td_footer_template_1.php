ab3944a1e86e97914884274e43b806a26d3aeff3
cristeaioan <c.ioan12@yahoo.com>
2016-07-18 14:40:44 +0300

    Archive page

--- parts/footer/td_footer_template_1.php
+++ parts/footer/td_footer_template_1.php
@@ -1,4 +1,20 @@
 <div class="td-footer-wrapper td-container-wrap">
+<!--    <div class="td-container">-->
+<!--        <div class="td-pb-row">-->
+<!--            <div class="td-pb-span4">-->
+<!--                --><?php //dynamic_sidebar('Footer 1'); ?>
+<!--            </div>-->
+<!---->
+<!--            <div class="td-pb-span4">-->
+<!--                --><?php //dynamic_sidebar('Footer 2'); ?>
+<!--            </div>-->
+<!---->
+<!--            <div class="td-pb-span4">-->
+<!--                --><?php //dynamic_sidebar('Footer 3'); ?>
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+
     <div class="td-container">
         <div class="td-pb-row">
             <?php locate_template('parts/footer/td_footer_extra_bottom.php', true); ?>
