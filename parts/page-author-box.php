609967eb8a99b16056f658408e03d97e4b9a44f1
cristeaioan <c.ioan12@yahoo.com>
2016-07-18 10:38:59 +0300

    Author page

--- parts/page-author-box.php
+++ parts/page-author-box.php
@@ -10,21 +10,17 @@ global $part_cur_auth_obj;
 
 ?>
 <div class="author-box-wrap td-author-page">
-
-    <?php  echo get_avatar($part_cur_auth_obj->user_email, '96'); ?>
     <div class="desc">
-
-
         <div class="td-author-counters">
             <span class="td-author-post-count">
-                <?php echo count_user_posts($part_cur_auth_obj->ID). ' '  . __td('POSTS', TD_THEME_NAME);?>
+                <?php echo '<span class="count">' . count_user_posts($part_cur_auth_obj->ID) . '</span>' . __td('posts', TD_THEME_NAME);?>
             </span>
 
             <span class="td-author-comments-count">
                 <?php
                 $comment_count = $wpdb->get_var($wpdb->prepare("SELECT COUNT(*) AS total FROM $wpdb->comments WHERE comment_approved = 1 AND user_id = %d", $part_cur_auth_obj->ID));
 
-                echo $comment_count . ' '  . __td('COMMENTS', TD_THEME_NAME);
+                echo '<span class="count">' .  $comment_count . '</span>' . __td('comments', TD_THEME_NAME);
                 ?>
             </span>
 
@@ -33,13 +29,7 @@ global $part_cur_auth_obj;
         <?php
         if (!empty($part_cur_auth_obj->user_url)) {
             echo '<div class="td-author-url"><a href="' . esc_url($part_cur_auth_obj->user_url) . '">' . esc_url($part_cur_auth_obj->user_url) . '</a></div>';
-        }
-
-        echo $part_cur_auth_obj->description;
-
-        ?>
-
-
+        } ?>
 
         <div class="td-author-social">
             <?php
@@ -53,6 +43,10 @@ global $part_cur_auth_obj;
             }
             ?>
         </div>
+    </div>
+
+    <div class="td-author-description">
+        <?php echo $part_cur_auth_obj->description; ?>
     </div>
 
     <div class="clearfix"></div>
