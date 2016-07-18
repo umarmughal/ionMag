3dc89f458e8035c10b8e234c9c6eb33258a9198a
marius <pmarius05@yahoo.com>
2016-07-18 17:38:50 +0300

    modal login

--- parts/header/td-login-modal.php
+++ parts/header/td-login-modal.php
@@ -14,7 +14,7 @@ if($users_can_register == 1){
     $users_can_register_form = '
                 <a href="#" class="td-back-button"><i class="td-icon-modal-back"></i></a>
                 <div id="td-register-div" class="td-login-form-div td-display-none">
-                    <div class="td-login-panel-title">Create an account</div>
+                    <div class="td-login-panel-title">' . __td('Create an account', TD_THEME_NAME) . '</div>
                     <div class="td-login-panel-descr">' . __td('Welcome! Register for an account', TD_THEME_NAME) .'</div>
                     <div class="td_display_err"></div>
                     <div class="td-login-inputs"><input class="td-login-input" type="text" name="register_email" id="register_email" value="" required><label>' . __td('your email', TD_THEME_NAME) .'</label></div>
@@ -29,7 +29,7 @@ echo '
                     <div class="td-login-wrap">
 
                         <div id="td-login-div" class="td-login-form-div td-display-block">
-                            <div class="td-login-panel-title">Sign in</div>
+                            <div class="td-login-panel-title">' . __td('Sign in', TD_THEME_NAME) . '</div>
                             <div class="td-login-panel-descr">' . __td('Welcome! Log into your account', TD_THEME_NAME) .'</div>
                             <div class="td_display_err"></div>
                             <div class="td-login-inputs"><input class="td-login-input" type="text" name="login_email" id="login_email" value="" required><label>' . __td('your username', TD_THEME_NAME) .'</label></div>
@@ -42,7 +42,7 @@ echo '
                         ' . $users_can_register_form . '
 
                          <div id="td-forgot-pass-div" class="td-login-form-div td-display-none">
-                            <div class="td-login-panel-title">Password recovery</div>
+                            <div class="td-login-panel-title">' . __td('Password recovery', TD_THEME_NAME) . '</div>
                             <div class="td-login-panel-descr">' . __td('Recover your password', TD_THEME_NAME) .'</div>
                             <div class="td_display_err"></div>
                             <div class="td-login-inputs"><input class="td-login-input" type="text" name="forgot_email" id="forgot_email" value="" required><label>' . __td('your email', TD_THEME_NAME) .'</label></div>
