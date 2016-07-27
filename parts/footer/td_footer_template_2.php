1d6ae741ea2bdb1fb6cda16ce541a80a885c1768
cristeaioan <c.ioan12@yahoo.com>
2016-07-27 11:23:47 +0300

    Footer & modules

--- parts/footer/td_footer_template_2.php
+++ parts/footer/td_footer_template_2.php
@@ -2,14 +2,40 @@
     <div class="td-container">
         <div class="td-pb-row">
             <div class="td-pb-span4">
+                <?php
+                td_util::vc_set_column_number(1);
+
+                echo td_global_blocks::get_instance('td_block_3')->render(array(
+                    'custom_title' => __td('EDITOR PICKS', TD_THEME_NAME),
+                    'limit' => 2,
+                    'sort' => 'featured'
+                ));
+                ?>
                 <?php dynamic_sidebar('Footer 1'); ?>
             </div>
 
             <div class="td-pb-span4">
+                <?php
+                td_util::vc_set_column_number(1);
+
+                echo td_global_blocks::get_instance('td_block_3')->render(array(
+                    'custom_title' => __td('POPULAR POSTS', TD_THEME_NAME),
+                    'limit' => 2,
+                    'sort' => 'popular'
+                ));
+                ?>
                 <?php dynamic_sidebar('Footer 2'); ?>
             </div>
 
             <div class="td-pb-span4">
+                <?php
+                td_util::vc_set_column_number(1);
+
+                echo td_global_blocks::get_instance('td_block_popular_categories')->render(array(
+                    'custom_title' => __td('POPULAR CATEGORY', TD_THEME_NAME),
+                    'limit' => 6
+                ));
+                ?>
                 <?php dynamic_sidebar('Footer 3'); ?>
             </div>
         </div>
