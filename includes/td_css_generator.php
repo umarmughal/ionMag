933bfc0e27f335f9633848332f9518c96927c848
marius <pmarius05@yahoo.com>
2016-08-19 10:05:23 +0300

    big grids css

--- includes/td_css_generator.php
+++ includes/td_css_generator.php
@@ -109,7 +109,7 @@ function td_css_generator() {
     }
     
     @media (min-width: 1019px) {
-      .td-grid-style-1.td-hover-1 .td-big-grid-post:hover .td-post-category {
+      .td_block_big_grid_1 .td-big-grid-post:hover .td-post-category {
         background-color: @theme_color;
       }
     }
