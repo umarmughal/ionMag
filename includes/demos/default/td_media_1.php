fd055ba97a22f06655e519bb17c05436f2bd6cf8
cristianpantazescu <c_pantazescu@yahoo.com>
2016-08-31 13:52:03 +0300

    commit

--- includes/demos/default/td_media_1.php
+++ includes/demos/default/td_media_1.php
@@ -1,14 +1,9 @@
 <?php
-//homepage images
-td_demo_media::add_image_to_media_gallery('td_pic_1',                   "http://demo_content.tagdiv.com/Newspaper_6/default/1.jpg");
-td_demo_media::add_image_to_media_gallery('td_pic_2',                   "http://demo_content.tagdiv.com/Newspaper_6/default/2.jpg");
-td_demo_media::add_image_to_media_gallery('td_pic_3',                   "http://demo_content.tagdiv.com/Newspaper_6/default/3.jpg");
-td_demo_media::add_image_to_media_gallery('td_pic_4',                   "http://demo_content.tagdiv.com/Newspaper_6/default/4.jpg");
+/**
+ * Created by ra on 6/13/2015.
+ */
 
-
-
-
-
-
-
-
+td_demo_media::add_image_to_media_gallery('td_pic_1',                   "http://demo_content.tagdiv.com/ion_mag/default/1.jpg");
+td_demo_media::add_image_to_media_gallery('td_pic_2',                   "http://demo_content.tagdiv.com/ion_mag/default/2.jpg");
+td_demo_media::add_image_to_media_gallery('td_pic_3',                   "http://demo_content.tagdiv.com/ion_mag/default/3.jpg");
+td_demo_media::add_image_to_media_gallery('td_pic_4',                   "http://demo_content.tagdiv.com/ion_mag/default/4.jpg");
--- includes/demos/default/td_media_5.php
+++ includes/demos/default/td_media_5.php
@@ -2,8 +2,11 @@
 /**
  * Created by ra on 6/13/2015.
  */
-td_demo_media::add_image_to_media_gallery('td_pic_17',                  "http://demo_content.tagdiv.com/Newspaper_6/default/17.jpg");
-td_demo_media::add_image_to_media_gallery('td_pic_18',                  "http://demo_content.tagdiv.com/Newspaper_6/default/18.jpg");
-//post images
-td_demo_media::add_image_to_media_gallery('td_pic_p1',                  "http://demo_content.tagdiv.com/Newspaper_6/default/p1.jpg");
-td_demo_media::add_image_to_media_gallery('td_pic_p2',                  "http://demo_content.tagdiv.com/Newspaper_6/default/p2.jpg");
+
+//single images
+td_demo_media::add_image_to_media_gallery('td_pic_infinite1',                "http://demo_content.tagdiv.com/ion_mag/default/infinite1.jpg");
+td_demo_media::add_image_to_media_gallery('td_pic_infinite2',                "http://demo_content.tagdiv.com/ion_mag/default/infinite2.jpg");
+td_demo_media::add_image_to_media_gallery('td_pic_infinite3',                "http://demo_content.tagdiv.com/ion_mag/default/infinite3.jpg");
+td_demo_media::add_image_to_media_gallery('td_pic_infinite4',                "http://demo_content.tagdiv.com/ion_mag/default/infinite4.jpg");
+td_demo_media::add_image_to_media_gallery('td_pic_infinite5',                "http://demo_content.tagdiv.com/ion_mag/default/infinite5.jpg");
+td_demo_media::add_image_to_media_gallery('td_pic_infinite6',                "http://demo_content.tagdiv.com/ion_mag/default/infinite6.jpg");
