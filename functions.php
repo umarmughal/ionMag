926a69fd8f85db03bd9748a2e3c5a53089c63d09
Ra <ra_coder@yahoo.com>
2016-10-24 14:19:10 +0300

    wp_booster check with error message - removed - does not work with symbilic links on windows :((((

--- functions.php
+++ functions.php
@@ -13,10 +13,10 @@ add_action('td_global_after', array('td_config', 'on_td_global_after_config'), 9
 
 
 // check and load the wp_booster framework
-if (!file_exists('includes/wp_booster/td_wp_booster_functions.php')) {
-    echo ':( wp_booster Framework not found! The framework should be in ' . TD_THEME_NAME . '/includes/wp_booster';
-    die;
-}
+//if (!file_exists('includes/wp_booster/td_wp_booster_functions.php')) {
+//    echo ':( wp_booster Framework not found! The framework should be in ' . TD_THEME_NAME . '/includes/wp_booster';
+//    die;
+//}
 require_once('includes/wp_booster/td_wp_booster_functions.php');
 
 require_once('includes/td_css_generator.php');
