7f2f48a921d11865ec910795690e11b0cab37969
marius <pmarius05@yahoo.com>
2016-11-09 14:23:36 +0200

    mega menu css fix

--- includes/td_config.php
+++ includes/td_config.php
@@ -537,7 +537,7 @@ class td_config {
                 'width' => 245,
                 'height' => 184,
                 'crop' => array('center', 'top'),
-                'post_format_icon_size' => 'small',
+                'post_format_icon_size' => 'large',
                 'used_on' => array(
                     'Mega menu'
                 ),
